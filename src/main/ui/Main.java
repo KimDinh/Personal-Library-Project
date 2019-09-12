d9ed02029251e622479eddbb36289ee5310fe83d
Kim Dinh <hoangkimidinh1608@gmail.com>
2019-09-12 10:51:38 -0700

    Deliverable 1

---
+++ src/main/ui/Main.java
@@ -0,0 +1,16 @@
+package ui;
+
+public class Main {
+    public static void main(String[] args){
+        sayHello();
+        seeProgress();
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello! This is a personal library application.");
+    }
+
+    public static void seeProgress(){
+        System.out.println("The application is still under progress. Please come back later.");
+    }
+}
